e319d4d42e2dde6d73a8afbe3e24aa6be24fd9bc
Otavio Marques <marquesotavio251204@gmail.com>
2022-10-30 11:54:06 -0300

    perguntas com o js

---
+++ public/src/js/dialog.js
@@ -0,0 +1,10 @@
+const  button = document.querySelector("button")
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialog button")
+
+button.onclick = function(){
+    modal.showModal()
+}
+buttonClose.onclick = function(){
+    modal.close()
+}
